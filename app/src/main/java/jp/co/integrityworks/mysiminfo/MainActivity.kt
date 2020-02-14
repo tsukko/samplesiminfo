7781f900bc2cf00b4f94f31317c7a21a2d07c31c
tsukko <tsukko@gmail.com>
2020-02-14 16:35:47 +0900

    update targetSDK to 29.

--- app/src/main/java/jp/co/integrityworks/mysiminfo/MainActivity.kt
+++ app/src/main/java/jp/co/integrityworks/mysiminfo/MainActivity.kt
@@ -1,50 +1,47 @@
 package jp.co.integrityworks.mysiminfo
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
 import android.telephony.TelephonyManager
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var mPreferenceUtil: PreferenceUtil? = null
+    private val PERMISSIONS_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Logger.debug(javaClass.simpleName, "onCreate")
+        setContentView(R.layout.activity_main)
 
-        Logger.debug(javaClass.getSimpleName(), "onCreate")
-        mPreferenceUtil = PreferenceUtil(this)
-
-        Logger.debug(javaClass.getSimpleName(), "Preference:" + mPreferenceUtil!!.preference)
-        if (ContextCompat.checkSelfPermission(
-                        this, android.Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
-            // 許可されている時の処理
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            // 許可されている
             getDisplay()
         } else {
-            // 拒否されている時の処理
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_PHONE_STATE)) {
-                //拒否された時 Permissionが必要な理由を表示して再度許可を求めたり、機能を無効にしたりします。
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_PHONE_STATE), 0)
-            } else {
-                //まだ許可を求める前の時、許可を求めるダイアログを表示します。
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_PHONE_STATE), 0)
-            }
+            // 許可されていないので許可ダイアログを表示する
+            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), PERMISSIONS_REQUEST_CODE)
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
-            0 -> { //ActivityCompat#requestPermissions()の第2引数で指定した値
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            PERMISSIONS_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //許可された場合の処理
                     getDisplay()
                 } else {
                     //拒否された場合の処理
+                    Handler().post(Runnable {
+                        RuntimePermissionUtils().showAlertDialog(supportFragmentManager, "READ_PHONE_STATE")
+                    })
                 }
             }
         }
@@ -56,82 +53,37 @@ class MainActivity : AppCompatActivity() {
         return oTv
     }
 
-    override fun onStart() {
-        super.onStart()
-        Logger.debug(javaClass.getSimpleName(), "onStart")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Logger.debug(javaClass.getSimpleName(), "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Logger.debug(javaClass.getSimpleName(), "onDestroy")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Logger.debug(javaClass.getSimpleName(), "onPause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Logger.debug(javaClass.getSimpleName(), "onResume")
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        Logger.debug(javaClass.getSimpleName(), "onSaveInstanceState")
-        mPreferenceUtil!!.preference = "test"
-    }
-
+    @SuppressLint("MissingPermission", "HardwareIds")
     private fun getDisplay() {
-        //レイアウトの生成
-        val oLayout = LinearLayout(applicationContext)
-        //上から下にパーツを組み込む
-        oLayout.orientation = LinearLayout.VERTICAL
-        //画面の設定
-        setContentView(oLayout)
+//        //レイアウトの生成
+//        val oLayout = LinearLayout(this)
+//        //上から下にパーツを組み込む
+//        oLayout.orientation = LinearLayout.VERTICAL
+//        //画面の設定
+//        setContentView(oLayout)
 
         //端末情報取得クラス:TelephonyManager生成
         val telMgr = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-
-
-// deviceID
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        //String deviceid = telMgr.getDeviceId();
         //電話番号
-        val phoneNumber = telMgr.line1Number
+        phoneNumber.text = telMgr.line1Number
         //SIM国別コード
-        val simCountryIso = telMgr.simCountryIso
+        simCountryIso.text = telMgr.simCountryIso
         //SIMシリアルナンバー
-        val simSerialNumber = telMgr.simSerialNumber
+        simSerialNumber.text = telMgr.simSerialNumber
         // 携帯端末固有ID
-        val deviceId = telMgr.deviceId
+        deviceId.text = telMgr.deviceId
+        //AndroidIDの取得
+        androidId.text = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+        // MCC+MNC
+        simOperator.text = telMgr.simOperator
+        // サービスプロバイダ名
+        simOperatorName.text = telMgr.simOperatorName
+        // SIM状態
+        simState.text = telMgr.simState.toString()
+        // ボイスメールナンバー
+        voiceMailNumber.text = telMgr.voiceMailNumber
+        // READ_PRIVILEGED_PHONE_STATE 必要
+        //       relativeLayout.addView(makeTextView("携帯端末固有(IMEI):${telMgr.imei}"))
 
-        //AndroiIDの取得
-        val sAndroid = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
-
-        oLayout.addView(makeTextView("Phone Number:$phoneNumber"))
-        oLayout.addView(makeTextView("SIM国別コード:$simCountryIso"))
-        oLayout.addView(makeTextView("SIMシリアルナンバー:$simSerialNumber"))
-        oLayout.addView(makeTextView("携帯端末固有(IMEI):$deviceId"))
-        oLayout.addView(makeTextView("Android ID:$sAndroid"))
-        oLayout.addView(makeTextView("MCC+MNC:" + telMgr.simOperator))
-        oLayout.addView(makeTextView("サービスプロバイダ名:" + telMgr.simOperatorName))
-        oLayout.addView(makeTextView("SIM状態:" + telMgr.simState))
-        oLayout.addView(makeTextView("ボイスメールナンバー:" + telMgr.voiceMailNumber))
     }
 }
--- app/src/main/java/jp/co/integrityworks/mysiminfo/SubActivity.kt
+++ app/src/main/java/jp/co/integrityworks/mysiminfo/SubActivity.kt
@@ -1,8 +1,8 @@
 package jp.co.integrityworks.mysiminfo
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_sub.*
 
