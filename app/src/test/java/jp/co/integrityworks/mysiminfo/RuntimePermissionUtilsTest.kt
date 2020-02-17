3a4005b402e14a7ff7d8077cb3e1b43e19374dc7
tsukko <tsukko@gmail.com>
2020-02-17 13:08:03 +0900

    ad test admob

---
+++ app/src/test/java/jp/co/integrityworks/mysiminfo/RuntimePermissionUtilsTest.kt
@@ -0,0 +1,22 @@
+package jp.co.integrityworks.mysiminfo
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class RuntimePermissionUtilsTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun showAlertDialog() {
+    }
+}
