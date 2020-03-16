83d18afb6d51579b608b091c6fcdc4d3c8d41da9
tsukko <tsukko@gmail.com>
2020-03-16 21:05:14 +0900

    use databinding.

--- app/src/main/java/jp/co/integrityworks/mysiminfo/MainActivity.kt
+++ app/src/main/java/jp/co/integrityworks/mysiminfo/MainActivity.kt
@@ -1,133 +1,95 @@
 package jp.co.integrityworks.mysiminfo
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.provider.Settings
-import android.telephony.TelephonyManager
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.reward.RewardedVideoAd
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import jp.co.integrityworks.mysiminfo.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private val PERMISSIONS_REQUEST_CODE = 100
-    private val mAd: RewardedVideoAd? = null
-    // A menu item view type.
-    private val MENU_ITEM_VIEW_TYPE = 0
-
-    // The unified native ad view type.
-    private val UNIFIED_NATIVE_AD_VIEW_TYPE = 1
+    private var binding: ActivityMainBinding? = null
+    //    private val mAd: RewardedVideoAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Logger.debug(javaClass.simpleName, "onCreate")
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding?.lifecycleOwner = this
+        val viewModel = ViewModelProvider(this).get(TelViewModel::class.java)
 
         if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
             // 許可されている
-            getDisplay()
+            viewModel.initParameters(applicationContext) // 初期表示時のデータ処理
         } else {
             // 許可されていないので許可ダイアログを表示する
-            requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), PERMISSIONS_REQUEST_CODE)
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_PHONE_STATE),
+                PERMISSIONS_REQUEST_CODE
+            )
         }
 
-        MobileAds.initialize(this, BuildConfig.admob_app_id)
-        val adRequest: AdRequest = AdRequest.Builder().build()
-        adView.loadAd(adRequest)
+        binding?.telViewModel = viewModel
 
-        // ad's lifecycle: loading, opening, closing, and so on
-        // ad's lifecycle: loading, opening, closing, and so on
-        adView.adListener = object : AdListener() {
-            override fun onAdLoaded() {
-                Log.d("debug", "Code to be executed when an ad finishes loading.")
-            }
-
-            override fun onAdFailedToLoad(errorCode: Int) {
-                Log.d("debug", "Code to be executed when an ad request fails.")
-            }
-
-            override fun onAdOpened() {
-                Log.d("debug", "Code to be executed when an ad opens an overlay that covers the screen.")
-            }
-
-            override fun onAdLeftApplication() {
-                Log.d("debug", "Code to be executed when the user has left the app.")
-            }
-
-            override fun onAdClosed() {
-                Log.d("debug", "Code to be executed when when the user is about to return to the app after tapping on an ad.")
-            }
-        }
+//        MobileAds.initialize(this, BuildConfig.admob_app_id)
+//        val adRequest: AdRequest = AdRequest.Builder().build()
+//        adView.loadAd(adRequest)
+//
+//        // ad's lifecycle: loading, opening, closing, and so on
+//        adView.adListener = object : AdListener() {
+//            override fun onAdLoaded() {
+//                Log.d("debug", "Code to be executed when an ad finishes loading.")
+//            }
+//
+//            override fun onAdFailedToLoad(errorCode: Int) {
+//                Log.d("debug", "Code to be executed when an ad request fails.")
+//            }
+//
+//            override fun onAdOpened() {
+//                Log.d(
+//                    "debug",
+//                    "Code to be executed when an ad opens an overlay that covers the screen."
+//                )
+//            }
+//
+//            override fun onAdLeftApplication() {
+//                Log.d("debug", "Code to be executed when the user has left the app.")
+//            }
+//
+//            override fun onAdClosed() {
+//                Log.d(
+//                    "debug",
+//                    "Code to be executed when when the user is about to return to the app after tapping on an ad."
+//                )
+//            }
+//        }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             PERMISSIONS_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //許可された場合の処理
-                    getDisplay()
+                    binding?.telViewModel?.initParameters(applicationContext)
                 } else {
                     //拒否された場合の処理
                     Handler().post(Runnable {
-                        RuntimePermissionUtils().showAlertDialog(supportFragmentManager, "READ_PHONE_STATE")
+                        RuntimePermissionUtils().showAlertDialog(
+                            supportFragmentManager,
+                            "READ_PHONE_STATE"
+                        )
                     })
                 }
             }
         }
-    }
-
-    private fun makeTextView(sTitle: String): TextView {
-        val oTv = TextView(this)
-        oTv.text = sTitle
-        return oTv
-    }
-
-    @SuppressLint("MissingPermission", "HardwareIds")
-    private fun getDisplay() {
-//        //レイアウトの生成
-//        val oLayout = LinearLayout(this)
-//        //上から下にパーツを組み込む
-//        oLayout.orientation = LinearLayout.VERTICAL
-//        //画面の設定
-//        setContentView(oLayout)
-
-        //端末情報取得クラス:TelephonyManager生成
-        val telMgr = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        //電話番号
-        phoneNumber.text = telMgr.line1Number
-        //SIM国別コード
-        simCountryIso.text = telMgr.simCountryIso
-        //SIMシリアルナンバー
-        // 携帯端末固有ID
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            simSerialNumber.text = "not support at OS 10"
-            deviceId.text = "not support at OS 10"
-        } else {
-            simSerialNumber.text = telMgr.simSerialNumber
-            deviceId.text = telMgr.deviceId
-        }
-        //AndroidIDの取得
-        androidId.text = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
-        // MCC+MNC
-        simOperator.text = telMgr.simOperator
-        // サービスプロバイダ名
-        simOperatorName.text = telMgr.simOperatorName
-        // SIM状態
-        simState.text = telMgr.simState.toString()
-        // ボイスメールナンバー
-        voiceMailNumber.text = telMgr.voiceMailNumber
-        // READ_PRIVILEGED_PHONE_STATE 必要
-        //       relativeLayout.addView(makeTextView("携帯端末固有(IMEI):${telMgr.imei}"))
-
     }
 }
--- app/src/main/java/jp/co/integrityworks/mysiminfo/TelViewModel.kt
+++ app/src/main/java/jp/co/integrityworks/mysiminfo/TelViewModel.kt
@@ -1,12 +1,72 @@
 package jp.co.integrityworks.mysiminfo
 
-import androidx.databinding.ObservableField
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-class TelData(telData: List<String>) {
-    var telData = ObservableField<List<String>>()
-    var aaaa = "edelleeee"
+class TelViewModel : ViewModel() {
+    private val _line1Number = MutableLiveData("")
+    private val _simCountryIso = MutableLiveData("")
+    private val _simSerialNumber = MutableLiveData("")
+    private val _deviceId = MutableLiveData("")
+    private val _androidId = MutableLiveData("")
+    private val _simOperator = MutableLiveData("")
+    private val _simOperatorName = MutableLiveData("")
+    private val _simState = MutableLiveData("")
+    private val _voiceMailNumber = MutableLiveData("")
 
-    init {
-        this.telData.set(telData)
+    val line1Number: LiveData<String> = _line1Number
+    val simCountryIso: LiveData<String> = _simCountryIso
+    val simSerialNumber: LiveData<String> = _simSerialNumber
+    val deviceId: LiveData<String> = _deviceId
+    val androidId: LiveData<String> = _androidId
+    val simOperator: LiveData<String> = _simOperator
+    val simOperatorName: LiveData<String> = _simOperatorName
+    val simState: LiveData<String> = _simState
+    val voiceMailNumber: LiveData<String> = _voiceMailNumber
+
+    @SuppressLint("MissingPermission", "HardwareIds")
+    fun initParameters(context: Context) {
+        //端末情報取得クラス:TelephonyManager生成
+        val telMgr = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        var hasC = telMgr.hasCarrierPrivileges()
+
+        //電話番号
+        _line1Number.value = telMgr.line1Number
+        //SIM国別コード
+        _simCountryIso.value = telMgr.simCountryIso
+        //SIMシリアルナンバー
+        // 携帯端末固有ID
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            _simSerialNumber.value = "not support at OS 10"
+        } else {
+            _simSerialNumber.value = telMgr.simSerialNumber
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            _deviceId.value = "not support at OS 10"
+        } else {
+            _deviceId.value = telMgr.deviceId
+        }
+        //AndroidIDの取得
+        _androidId.value =
+            Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        // MCC+MNC
+        _simOperator.value = telMgr.simOperator
+        // サービスプロバイダ名
+        _simOperatorName.value = telMgr.simOperatorName
+        // SIM状態
+        _simState.value = telMgr.simState.toString()
+        // ボイスメールナンバー
+        _voiceMailNumber.value = telMgr.voiceMailNumber
+        // READ_PRIVILEGED_PHONE_STATE 必要
+        //       relativeLayout.addView(makeTextView("携帯端末固有(IMEI):${telMgr.imei}"))
+//        binding.telData = listOf(telMgr.line1Number)
     }
 }
