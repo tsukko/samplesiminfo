c4b4194c52d70d195328ad9192a945d84dc70a8a
tsukko <tsukko@gmail.com>
2021-02-11 03:04:27 +0900

    fix requestPermissions

--- app/src/main/java/jp/co/integrityworks/mysiminfo/MainActivity.kt
+++ app/src/main/java/jp/co/integrityworks/mysiminfo/MainActivity.kt
@@ -2,12 +2,13 @@ package jp.co.integrityworks.mysiminfo
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
@@ -21,40 +22,58 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val PERMISSIONS_REQUEST_CODE = 100
-    private var binding: ActivityMainBinding? = null
+    private val REQUEST_MULTI_PERMISSIONS = 200
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: TelViewModel
     //    private val mAd: RewardedVideoAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Logger.debug(javaClass.simpleName, "onCreate")
+        Logger.debug(TAG, "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         supportActionBar?.title =
                 if (BuildConfig.DEBUG) getString(R.string.app_name) + " (deb)" else getString(R.string.app_name)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding?.lifecycleOwner = this
-        val viewModel = ViewModelProvider(this).get(TelViewModel::class.java)
+        binding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this).get(TelViewModel::class.java)
 
-        // TODO パーミッション許可周りの実装を見直す、今は以下のエラーが発生する
-        // Caused by: java.lang.SecurityException: getLine1NumberForDisplay: Neither user 10298 nor current process has android.permission.READ_PHONE_STATE, android.permission.READ_SMS, or android.permission.READ_PHONE_NUMBERS
-        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
-            // 許可されている
-            viewModel.initParameters(applicationContext) // 初期表示時のデータ処理
-        } else {
-            // 許可されていないので許可ダイアログを表示する
-            requestPermissions(
-                    arrayOf(Manifest.permission.READ_PHONE_STATE),
-                    PERMISSIONS_REQUEST_CODE
-            )
-        }
+//        val checkPermission: String = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//            Manifest.permission.READ_PHONE_NUMBERS
+//        } else {
+//            Manifest.permission.READ_PHONE_STATE
+//        }
+//        if (checkSelfPermission(checkPermission) == PackageManager.PERMISSION_GRANTED) {
+//            // 許可されている
+//            viewModel.initParameters(applicationContext) // 初期表示時のデータ処理
+//        } else {
+//            // 許可されていないので許可ダイアログを表示する
+//            requestPermissions(
+//                    arrayOf(checkPermission),
+//                    PERMISSIONS_REQUEST_CODE
+//            )
+//        }
+//        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+//            // 許可されている
+////            viewModel.initParameters(applicationContext) // 初期表示時のデータ処理
+//        } else {
+//            // 許可されていないので許可ダイアログを表示する
+//            requestPermissions(
+//                    arrayOf(Manifest.permission.READ_PHONE_STATE),
+//                    PERMISSIONS_REQUEST_CODE
+//            )
+//        }
+        checkMultiPermissions()
 
-        binding?.telViewModel = viewModel
+        binding.telViewModel = viewModel
 
         MobileAds.initialize(this, BuildConfig.admob_app_id)
         val adRequest: AdRequest = AdRequest.Builder().build()
-        binding?.adView?.loadAd(adRequest)
+        binding.adView.loadAd(adRequest)
 
         // ad's lifecycle: loading, opening, closing, and so on
-        binding?.adView?.adListener = object : AdListener() {
+        binding.adView.adListener = object : AdListener() {
             override fun onAdLoaded() {
                 Logger.debug(TAG, "Code to be executed when an ad finishes loading.")
             }
@@ -85,34 +104,23 @@ class MainActivity : AppCompatActivity() {
 
     // TODO パーミッション許可周りの実装を見直す
     private fun checkMultiPermissions() {
-        // 位置情報の Permission
-        val permissionLocation = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-
-        // 外部ストレージ書き込みの Permission
-        val permissionExtStorage = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
         val reqPermissions: ArrayList<String> = ArrayList()
-
-        // 位置情報の Permission が許可されているか確認
-        if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
-        } else {
-            reqPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (checkSelfPermission(Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED) {
+                reqPermissions.add(Manifest.permission.READ_PHONE_NUMBERS)
+            }
         }
-
-        // 外部ストレージ書き込みが許可されているか確認
-        if (permissionExtStorage == PackageManager.PERMISSION_GRANTED) {
-        } else {
-            reqPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            reqPermissions.add(Manifest.permission.READ_PHONE_STATE)
         }
 
         // 未許可
-        if (!reqPermissions.isEmpty()) {
-            ActivityCompat.requestPermissions(this,
+        if (reqPermissions.isNotEmpty()) {
+            requestPermissions(
                     reqPermissions.toArray(arrayOfNulls(0)),
                     REQUEST_MULTI_PERMISSIONS)
         } else {
-            startLocationService()
+            viewModel.initParameters(applicationContext) // 初期表示時のデータ処理
         }
     }
 
@@ -125,15 +133,58 @@ class MainActivity : AppCompatActivity() {
             PERMISSIONS_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //許可された場合の処理
-                    binding?.telViewModel?.initParameters(applicationContext)
+                    binding.telViewModel?.initParameters(applicationContext)
+//                        if (permissions[0] == Manifest.permission.READ_PHONE_STATE) {
+//
+//                        }
+//                    else {
+//                            Logger.debug(TAG, "aaaaaaaaaaaaaa")
+//                        }
                 } else {
                     //拒否された場合の処理
-                    Handler().post(Runnable {
+                    Handler(Looper.getMainLooper()).post {
                         RuntimePermissionUtils().showAlertDialog(
                                 supportFragmentManager,
                                 "READ_PHONE_STATE"
                         )
-                    })
+                    }
+                }
+            }
+            REQUEST_MULTI_PERMISSIONS -> {
+                if (grantResults.isNotEmpty()) {
+                    var activeNum = 0
+                    for (i in permissions.indices) {
+                        if (permissions[i] == Manifest.permission.READ_PHONE_NUMBERS) {
+                            if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                                Logger.debug(TAG, "READ_PHONE_NUMBERS: GRANTED")
+                                activeNum++
+                            } else {
+                                // 拒否された時の対応
+                                Handler(Looper.getMainLooper()).post {
+                                    RuntimePermissionUtils().showAlertDialog(
+                                            supportFragmentManager,
+                                            "READ_PHONE_NUMBERS"
+                                    )
+                                }
+                            }
+                        } else if (permissions[i] == Manifest.permission.READ_PHONE_STATE) {
+                            if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                                Logger.debug(TAG,"READ_PHONE_STATE: GRANTED")
+                                activeNum++
+                            } else {
+                                // 拒否された時の対応
+                                Handler(Looper.getMainLooper()).post {
+                                    RuntimePermissionUtils().showAlertDialog(
+                                            supportFragmentManager,
+                                            "READ_PHONE_STATE"
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    if (activeNum == 2) {
+                        binding.telViewModel?.initParameters(applicationContext)
+                    }
                 }
             }
         }
